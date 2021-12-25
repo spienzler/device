9d66b1250a4724c1197b8efe64cb5768eee29436
psunix <ps@s-it.ch>
2021-12-25 20:07:15 +0100

    Add reolink snap img script

---
+++ bin/rl-snap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source rl.env
+
+# login
+JSON=$(curl -s -d "[{\"cmd\":\"Login\",\"action\":0,\"param\":{\"rs\": \"abcd\", \"User\":{\"userName\":\"$USER\",\"password\":\"$PW\"}}}]" $HOST/api.cgi?cmd=Login)
+TOKEN=$(echo $JSON | jq -r .[0].value.Token.name)
+
+# fetch picture
+wget -O $1 "http://$HOST/cgi-bin/api.cgi?cmd=Snap&channel=0&rs=YSkd_RRrShvyAMZv&token=$TOKEN"
